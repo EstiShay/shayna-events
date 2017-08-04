6bcc045af4e5d2cf28a856b8f915f7b558050d7a
Esti Shay <esti.r.shay@gmail.com>
2017-08-04 09:09:22 -0700

    add Event constructor

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -6,9 +6,9 @@ public class Event {
     public String beverages;
     public String entertainment;
 
-//    public Event(String food, String beverages, String entertainment) {
-//        this.food = food;
-//        this.beverages = beverages;
-//        this.entertainment = entertainment;
-//    }
+    public Event(String food, String beverages, String entertainment) {
+        this.food = food;
+        this.beverages = beverages;
+        this.entertainment = entertainment;
+    }
 }

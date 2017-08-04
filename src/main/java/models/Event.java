a0d07bedcb9f80354618ff840ffe6fda082ced26
Esti Shay <esti.r.shay@gmail.com>
2017-08-04 11:35:09 -0700

    update Event constructor with missing attribute

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -5,10 +5,12 @@ public class Event {
     public String food;
     public String beverages;
     public String entertainment;
+    public Integer guests;
 
-    public Event(String food, String beverages, String entertainment) {
+    public Event(String food, String beverages, String entertainment, Integer guests) {
         this.food = food;
         this.beverages = beverages;
         this.entertainment = entertainment;
+        this.guests = guests;
     }
 }

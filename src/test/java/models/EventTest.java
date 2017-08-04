cb8966fb0d5866730f2b91bc32127590919a79fb
Esti Shay <esti.r.shay@gmail.com>
2017-08-04 09:06:23 -0700

    create test to verify constructor will instantiate correctly

--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -1,7 +1,17 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EventTest {
+
+    @Test
+    public void Event_instantiatesCorrectly() throws Exception {
+        Event testEvent = new Event("appetizers", "simple", "dj");
+        assertEquals(true, testEvent instanceof Event);
+    }
+
+
 
 }

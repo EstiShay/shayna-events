9ec3633cbee150d6e8b4de9dae41978deb57f97b
Esti Shay <esti.r.shay@gmail.com>
2017-08-04 09:07:19 -0700

    create Event constructor

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,4 +1,14 @@
 package models;
 
 public class Event {
+
+    public String food;
+    public String beverages;
+    public String entertainment;
+
+//    public Event(String food, String beverages, String entertainment) {
+//        this.food = food;
+//        this.beverages = beverages;
+//        this.entertainment = entertainment;
+//    }
 }

9f4b44b51e2c0c821786ede6c3702c07f9e74e62
Esti Shay <esti.r.shay@gmail.com>
2017-08-04 16:40:37 -0700

    create getter for guests attribute

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -28,48 +28,56 @@ public class Event {
         return entertainment;
     }
 
-//    public int calculateCost() {
-//        Integer foodTotal = 0;
-//        Integer bevTotal = 0;
-//        Integer entTotal = 0;
-//        if (food.equals("Salmon BBQ")){
-//            foodTotal += (20 * guests);
-//        } else if (food.equals("Appetizers")){
-//            foodTotal += (10 * guests);
-//        } else if (food.equals("Mediterranean Lunch")){
-//            foodTotal += (15 * guests);
-//        } else if (food.equals("Shabbat Dinner")){
-//            foodTotal += (25 * guests);
-//        } else {
-//            System.out.println("Cost calculation error - food input");
-//        }
-////        System.out.println(String.format("Your selection %s will cost $%d.", food, foodTotal));
-//
-//        if (beverages.equals("Punch Service")) {
-//            bevTotal += (3 * guests);
-//        } else if (beverages.equals("Alcohol Service")) {
-//            bevTotal += (7 * guests);
-//        } else if (beverages.equals("Full Bar")){
-//            bevTotal += (15 * guests);
-//        } else {
-//            System.out.println("Cost calculation error - beverage input");
-//        }
-////        System.out.println(String.format("Your selection %s will cost $%d.", beverages, bevTotal));
-//
-//        if (entertainment.equals("DJ")) {
-//            entTotal += 200;
-//        } else if (entertainment.equals("Live Band")) {
-//            entTotal += 2000;
-//        } else if (entertainment.equals("String Quartet")) {
-//            entTotal += 1000;
-//        } else if (entertainment.equals("None")) {
-//            entTotal += 0;
-//        } else {
-//            System.out.println("Cost calculation error - entertainment input");
-//        }
-////        System.out.println(String.format("Your selection %s will cost $%d.", entertainment, entTotal));
-//
-//        int total = foodTotal + bevTotal + entTotal;
-//        return total;
-//    }
+    public Integer getGuests() {
+        return guests;
+    }
+
+    public String getPerk() {
+        return perk;
+    }
+
+    public int calculateCost() {
+        Integer foodTotal = 0;
+        Integer bevTotal = 0;
+        Integer entTotal = 0;
+        if (food.equals("Salmon BBQ")){
+            foodTotal += (20 * guests);
+        } else if (food.equals("Appetizers")){
+            foodTotal += (10 * guests);
+        } else if (food.equals("Mediterranean Lunch")){
+            foodTotal += (15 * guests);
+        } else if (food.equals("Shabbat Dinner")){
+            foodTotal += (25 * guests);
+        } else {
+            System.out.println("Cost calculation error - food input");
+        }
+//        System.out.println(String.format("Your selection %s will cost $%d.", food, foodTotal));
+
+        if (beverages.equals("Punch Service")) {
+            bevTotal += (3 * guests);
+        } else if (beverages.equals("Alcohol Service")) {
+            bevTotal += (7 * guests);
+        } else if (beverages.equals("Full Bar")){
+            bevTotal += (15 * guests);
+        } else {
+            System.out.println("Cost calculation error - beverage input");
+        }
+//        System.out.println(String.format("Your selection %s will cost $%d.", beverages, bevTotal));
+
+        if (entertainment.equals("DJ")) {
+            entTotal += 200;
+        } else if (entertainment.equals("Live Band")) {
+            entTotal += 2000;
+        } else if (entertainment.equals("String Quartet")) {
+            entTotal += 1000;
+        } else if (entertainment.equals("None")) {
+            entTotal += 0;
+        } else {
+            System.out.println("Cost calculation error - entertainment input");
+        }
+//        System.out.println(String.format("Your selection %s will cost $%d.", entertainment, entTotal));
+
+        int total = foodTotal + bevTotal + entTotal;
+        return total;
+    }
 }
